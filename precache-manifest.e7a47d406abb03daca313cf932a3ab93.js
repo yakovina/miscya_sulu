df71603b1cb41b296df758b456ad6ba8a1637ccf
comfy <comfy@MacBook-Air-comfy.local>
2021-02-13 03:43:22 +0200

    fixes

---
+++ precache-manifest.e7a47d406abb03daca313cf932a3ab93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "310ab01d3abb9da16ae6",
+    "url": "/static/css/main.51b06a54.chunk.css"
+  },
+  {
+    "revision": "310ab01d3abb9da16ae6",
+    "url": "/static/js/main.ad0cb9fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3f9f81c52cf47311076",
+    "url": "/static/js/2.0845f9c5.chunk.js"
+  },
+  {
+    "revision": "e13277cb03e33e829b984dc69e79d891",
+    "url": "/static/media/ib-logo.e13277cb.svg"
+  },
+  {
+    "revision": "ede2f44539ab87001a96c1e54d62f470",
+    "url": "/static/media/tree.ede2f445.png"
+  },
+  {
+    "revision": "30ab269f974684a60273ee8c038072ea",
+    "url": "/static/media/bg2.30ab269f.jpeg"
+  },
+  {
+    "revision": "b97aff8cd17e6c6c9cf9ceeae3962a4c",
+    "url": "/index.html"
+  }
+];
